f9639e421e1d6e1b0672ced616478b4a3eddefa6
goldman2g5 <tima.radchenko.84@mail.ru>
2022-04-12 05:14:30 +0300

    Initial commit

---
+++ prac9/Operation.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace prac9;
+
+public class Operation
+{
+    public string Trigger;
+    public Func<double, double, double> Fn;
+    public static List<Operation> List = new List<Operation>();
+
+    public Operation(string trigger, Func<double, double, double> fn)
+    {
+        Trigger = trigger;
+        Fn = fn;
+        List.Add(this);
+    }
+
+    public static void CreateOperations()
+    {
+        double Sum(double x, double y)
+        {
+            return x + y;
+        }
+            
+        double Minus(double x, double y)
+        {
+            return x - y;
+        }
+            
+        double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+            
+        double Divide(double x, double y)
+        {
+            return x / y;
+        }
+            
+        double Divide2(double x, double y)
+        {
+            return x % y;
+        }
+            
+        double Degree(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        new Operation("+", Sum);
+        new Operation("-", Minus);
+        new Operation("*", Multiply);
+        new Operation("/", Divide);
+        new Operation("%", Divide2);
+        new Operation("^", Degree);
+    }
+}
---
+++ prac9/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Globalization;
+using prac9;
+
+public class Program
+{
+    private static void Main(string[] args)
+        {
+            Operation.CreateOperations();
+            
+            void Func(ref List<string> strLs, string value = "null")
+            {
+                Console.Clear();
+                Console.Write($"Input expression\n >> {(value == "null" ? "" : value)} ");
+                string input = Console.ReadLine().ToLower();
+
+                switch (input)
+                {
+                    case "exit":
+                        strLs.Add("exit");
+                        break;
+
+                    case "clear":
+                        strLs.Add("clear");
+                        Func(ref strLs);
+                        break;
+
+                    default:
+                        try
+                        {
+                            if (value != "null" & !Operation.List.Select(x => x.Trigger).ToList().Contains(input[0].ToString()))
+                            {
+                                Console.WriteLine("Input string was not in a correct format\nType 'clear' to work with a new number\nPress any key");
+                                strLs.Add("Input string was not in a correct format.");
+                                Console.ReadKey();
+                                Func(ref strLs, value);
+                            }
+                            if (value != "null")
+                            {
+                                input = $"{value}" + input;
+                            }
+                            var queue = new Queue<string>();
+                            string temp = "";
+                            foreach (string i in input.ToCharArray().Select(x => x.ToString()).Where(x => x != " ").ToList())
+                            {
+                                if (Operation.List.Select(x => x.Trigger).ToList().Contains(i))
+                                {
+                                    queue.Enqueue(temp);
+                                    queue.Enqueue(i);
+                                    temp = "";
+                                    continue; 
+                                }
+                                temp += i;
+                            }
+                            queue.Enqueue(temp);
+                            
+                            double x = Convert.ToDouble(queue.Dequeue());
+                            while (queue.Count != 0)
+                            {
+                                string t = queue.Dequeue();
+                                x = Operation.List.Where(x => x.Trigger == t).ToList()[0].Fn(x, Convert.ToDouble(queue.Dequeue()));
+
+                            }
+                            strLs.Add($"{input} = {value}");
+                            Func(ref strLs, Math.Round(x, 2).ToString());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"{e.Message}\nPress any key");
+                            strLs.Add($"{e.Message}");
+                            Console.ReadKey();
+                            Func(ref strLs);
+                        }
+                        break;
+                }
+            }
+
+            void Log()
+            {
+                try
+                {
+                    List<string> ls = new List<string>();
+                    Func(ref ls);
+                    string path = @$"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\\{DateTime.Now.ToLongDateString()}.txt";
+                    if (!File.Exists(path))
+                    {
+                        File.CreateText(path).Close();
+                    }
+                    using StreamWriter w = File.AppendText(path);
+                    w.WriteLine($"{DateTime.Now.ToLongTimeString()}");
+                    foreach (string i in ls)
+                    {
+                        w.WriteLine(i);   
+                    }
+                    w.WriteLine("");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e.Message}\nPress any key to retry");
+                    Console.ReadKey();
+                    Log();
+                }
+            }
+            
+            Log();
+        }
+        
+    }
